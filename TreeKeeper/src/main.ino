eac1aa522e94e6ed09173b87bbc65dbf28ab8cc1
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-11-04 13:09:26 +0530

    config error with vsc fixed

--- TreeKeeper/src/main.ino
+++ TreeKeeper/src/main.ino
@@ -1,18 +1,7 @@
 #include <Arduino.h>
 
-/*
-  TreeKeeper, a wildfire detection and early-warning system built on Arduino.
-  Avaialable on GitHub at (https://Github.com/Isuru2701/InternetofTrees)
-
-  Features:
-  - Detects the following via sensors:
-      - Temperature & Humidity
-      - Soil Moisture
-      - 
-*/
-
 void setup() {
-
+    pinMode(13, OUTPUT);
 }
 
 void loop() {
